fa2a23d1fdecc19998ef373d3e3089810a37111e
EakzIT <pilpaff@gmail.com>
2020-04-27 02:01:41 +0300

    .

--- CodeWars/python/6kyu_reversing_a_process.py
+++ CodeWars/python/6kyu_reversing_a_process.py
@@ -5,6 +5,8 @@ from string import ascii_lowercase as aLow
 def decode(r):
     i    = next(i for i,c in enumerate(r) if c.isalpha())
     n,r  = int(r[:i]), r[i:]
+    print('n',n,'\nr-',r)
+    print('alow',aLow)
     maps = {chr(97 + n*i % 26): c for i,c in enumerate(aLow)}
     return "Impossible to decode" if len(maps)!=26 else ''.join(maps[c] for c in r)
 
@@ -19,8 +21,9 @@ def decode(r):
         
 #     @test.it('Basic Tests')
 #     def basic_tests1():
-print(decode("1273409kuqhkoynvvknsdwljantzkpnmfgf")) # "uogbucwnddunktsjfanzlurnyxmx")
-print(decode("761328qockcouoqmoayqwmkkic")) # "Impossible to decode")
-print(decode("1544749cdcizljymhdmvvypyjamowl")) # "mfmwhbpoudfujjozopaugcb")
-print(decode("1122305vvkhrrcsyfkvejxjfvafzwpsdqgp")) # "rrsxppowmjsrclfljrajtybwviqb")
+print(decode('6015ekx'))
+# print(decode("1273409kuqhkoynvvknsdwljantzkpnmfgf")) # "uogbucwnddunktsjfanzlurnyxmx")
+# print(decode("761328qockcouoqmoayqwmkkic")) # "Impossible to decode")
+# print(decode("1544749cdcizljymhdmvvypyjamowl")) # "mfmwhbpoudfujjozopaugcb")
+# print(decode("1122305vvkhrrcsyfkvejxjfvafzwpsdqgp")) # "rrsxppowmjsrclfljrajtybwviqb")
         
