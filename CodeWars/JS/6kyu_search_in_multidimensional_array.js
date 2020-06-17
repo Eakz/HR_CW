411b31693fa9c306852cbb54da52431428974875
EakzIT <pilpaff@gmail.com>
2020-06-17 18:55:05 +0300

    .

--- CodeWars/JS/6kyu_search_in_multidimensional_array.js
+++ CodeWars/JS/6kyu_search_in_multidimensional_array.js
@@ -5,7 +5,3 @@ const locate = (arr,value)=> {
 console.log(locate(['a','b',['c','d',['e']]],'e')); // should return true
 console.log(locate(['a','b',['c','d',['e']]],'a')); // should return true
 console.log(locate(['a','b',['c','d',['e']]],'f')); // should return false
-console.log(locate([ [ [ [Array], [Array] ] ] ], ',0.8428168444390085'
-[ [ [ 0 ], [ [Array], [Array] ] ] ], ',0.8428168444390085'
-[ [ 0 ], [ [ 1 ], [ [Array], [Array] ] ] ], ',0.8428168444390085'
-[ 0 ] ',0.8428168444390085')); // should return true
