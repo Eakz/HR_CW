48939f43aa3546722c775c17c89166f6dac46c74
EakzIT <pilpaff@gmail.com>
2020-07-19 18:37:43 +0300

    comments to 7kyu solution

--- CodeWars/JS/7kyu_array_element_parity.js
+++ CodeWars/JS/7kyu_array_element_parity.js
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/5a092d9e46d843b9db000064/train/javascript
 
+// solution with filter stright forward search through iteration
 const solve = (arr) => arr.filter((e) => !arr.includes(e * -1))[0];
+
+// solution more logical solution - summing up the array
+const solve = arr => arr.reduce((acc,e)=>acc+e);
 
 console.log(solve([1, -1, 2, -2, 3])); //3);
 console.log(solve([-3, 1, 2, 3, -1, -4, -2])); //-4);
