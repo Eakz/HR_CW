d39a8e6dfbc0b31d0b04f89a4c8f728d13c2831a
EakzIT <pilpaff@gmail.com>
2020-07-19 19:36:05 +0300

    hash tables

---
+++ hackerrank/Interview_preparation_kit/dict_and_hashmaps/hash_tables_ransom_note.js
@@ -0,0 +1,52 @@
+// https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+    let result =note.every(a=>{
+        if(magazine.includes(a)){
+        magazine.splice(magazine.indexOf(a),1);
+        return true;
+        }else{
+        return false;
+        }
+        });
+    console.log(result?"Yes":"No");
+
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
