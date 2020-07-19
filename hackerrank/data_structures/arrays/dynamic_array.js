13f08aad5c1b623f854f3766a562f788600893b7
EakzIT <pilpaff@gmail.com>
2020-07-19 19:21:29 +0300

    HR dynamic array

---
+++ hackerrank/data_structures/arrays/dynamic_array.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+    let sl = {},
+    la=0,
+    las=[],
+    l=queries.length;
+
+    for(let i=0; i<l;i++){
+        let [a,b,c]=queries[i],
+        si=(b^la)%n;
+
+        if(a===1){
+            if(sl[si]){
+                sl[si].push(c)
+            }else{
+                sl[si]=[c]
+            }
+        }else if(a===2){
+            la=sl[si][c%sl[si].length];
+            las.push(la);
+        }
+    }
+    return las;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const q = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = dynamicArray(n, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
