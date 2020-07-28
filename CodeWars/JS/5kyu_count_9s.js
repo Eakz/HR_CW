e176210a2e3f92a771ba5496958c35831b7d2841
EakzIT <pilpaff@gmail.com>
2020-07-28 10:00:04 +0300

    5kyu without solution

---
+++ CodeWars/JS/5kyu_count_9s.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55143152820d22cdf00001bb/train/javascript
+
+// return the number of 9's used to count out 1 to n
+function number9(n) {}
+
+console.log(number9(1)); // 0, 'Nein!')
+console.log(number9(9)); // 1, 'Nein!')
+console.log(number9(100)); // 20, 'Nein!')
+console.log(number9(565754)); //275645, 'Nein!')
+console.log(number9(10000000000)); //10000000000, 'Nein!')
