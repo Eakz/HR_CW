4e662d4175f97e4dafe49b3c8129e555790dc344
EakzIT <pilpaff@gmail.com>
2020-07-30 17:11:21 +0300

    7kyu JS Codewars Mumbling

---
+++ CodeWars/JS/7kyu_mumbling.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+
+const accum = (s) =>
+    s
+        .split("")
+        .map((e, i) => `${e.toUpperCase()}${e.toLowerCase().repeat(i)}`)
+        .join("-");
+
+console.log(accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+console.log(accum("NyffsGeyylB")); // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+console.log(accum("MjtkuBovqrU")); // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+console.log(accum("EvidjUnokmM")); // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+console.log(accum("HbideVbxncC")); // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
