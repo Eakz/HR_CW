c75ff28a662730a5ff4f25978d4e4121f4c64a2c
EakzIT <pilpaff@gmail.com>
2020-08-01 23:38:11 +0300

    8kyu say hello py (as good night solution)

---
+++ CodeWars/python/8kyu_grasshoper_debug_say_hello.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/python
+
+def say_hello(name):
+    return f"Hello, {name}"
+
+
+print(say_hello('Mr. Spock'), 'Hello, Mr. Spock')
+print(say_hello('Captain Kirk'), 'Hello, Captain Kirk')
+print(say_hello('Liutenant Uhura'), 'Hello, Liutenant Uhura')
+print(say_hello('Dr. McCoy'), 'Hello, Dr. McCoy')
+print(say_hello('Mr. Scott'), 'Hello, Mr. Scott')
