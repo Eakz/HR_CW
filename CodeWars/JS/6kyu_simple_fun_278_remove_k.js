0ebd570a03b90bf28b74e3b383c2ce7fed7ca0ae
EakzIT <pilpaff@gmail.com>
2020-08-12 09:18:04 +0300

    6kyu simple fun 278 - remove K solution JS

---
+++ CodeWars/JS/6kyu_simple_fun_278_remove_k.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5919237fcdee8ddd25000082/train/javascript
+
+const removeK = (k) => k**2+((k+1)*k) / 2;
+
+console.log(removeK(2),7)
+  
+console.log(removeK(3),15)
+  
+console.log(removeK(10),155)
+  
+console.log(removeK(25),950)
+  
+console.log(removeK(700),735350)
+  
+console.log(removeK(1234),2284751)
+  
