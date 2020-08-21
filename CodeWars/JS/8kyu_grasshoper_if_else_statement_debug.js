65daccc6c5519404dd21f885d4291d7803fa739c
EakzIT <pilpaff@gmail.com>
2020-08-21 09:40:24 -0400

    8kyu grasshoper if else statement debug JS

---
+++ CodeWars/JS/8kyu_grasshoper_if_else_statement_debug.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57089707fe2d01529f00024a/train/javascript
+
+const checkAlive = health => (health > 0 ? true : false);
+
+console.log(checkAlive(5), true);
+console.log(checkAlive(0), false);
