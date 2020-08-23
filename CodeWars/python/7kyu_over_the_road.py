273062506eb115cb814142b14310fa3269783f7d
EakzIT <pilpaff@gmail.com>
2020-08-23 09:09:26 -0400

    7kyu over the road PY math odd/even problem

---
+++ CodeWars/python/7kyu_over_the_road.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/python
+
+def over_the_road(a, n):
+    return n*2-a+1
+
+
+print(over_the_road(1, 3), 6)
+print(over_the_road(3, 3), 4)
+print(over_the_road(2, 3), 5)
+print(over_the_road(3, 5), 8)
+print(over_the_road(7, 11), 16)
