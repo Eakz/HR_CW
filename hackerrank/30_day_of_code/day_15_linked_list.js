5a713a6fd7a97f2a8cb8e0209348449d1fac3e00
EakzIT <pilpaff@gmail.com>
2020-09-25 10:31:20 +0300

    30 day day15 JS solution :octocat:

--- hackerrank/30_day_of_code/day_15_linked_list.js
+++ hackerrank/30_day_of_code/day_15_linked_list.js
@@ -1,2 +1,42 @@
 // https://www.hackerrank.com/challenges/30-linked-list/problem
 
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+function Solution(){
+
+	this.insert=function io(head,data){
+        let cursor=head;
+        if(head === null){
+            return new Node( data);
+        }
+        while(head.next!==null){
+            head=head.next
+        }
+        if(head.next === null){
+            head.next = new Node(data);
+            return cursor
+
+    }}
+
+	this.display=function(head){
