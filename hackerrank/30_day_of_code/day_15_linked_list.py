4513623383335c6f43155b64b11e25b5e7918378
EakzIT <pilpaff@gmail.com>
2020-09-25 10:14:18 +0300

    s

---
+++ hackerrank/30_day_of_code/day_15_linked_list.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        if head is None:
+            head = Node(data)
+            self.tail = head
+        else:
+            node = Node(data)
+            self.tail.next = node
+            self.tail = node
+        return head
+
+
+mylist = Solution()
