3b2a6f6108d816e84b9c3989666abd9bde6437ff
EakzIT <pilpaff@gmail.com>
2020-10-01 18:06:30 +0300

    HR 30 day day 21 generics - c# solution was fun!

---
+++ hackerrank/30_day_of_code/day_21_generics.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Printer
+{
+
+
+
+        public static void PrintArray<T>(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
