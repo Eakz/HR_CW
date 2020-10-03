0b52081b8ba210a0afafb20641a9d39f95c94030
EakzIT <pilpaff@gmail.com>
2020-10-03 14:06:57 +0300

    ...

--- hackerrank/problem_solving/algorithms/implementation/drawing_book.js
+++ hackerrank/problem_solving/algorithms/implementation/drawing_book.js
@@ -1,6 +1,6 @@
 // https://www.hackerrank.com/challenges/drawing-book/problem
-function pageCount(n, p) {
-    const p = Math.floor(p / 2);
+function pageCount(n, pr) {
+    const p = Math.floor(pr / 2);
     const t = Math.floor(n / 2);
     return Math.min(p, t - p);
 }
