c1c1d85ffcdc57969fb5e18c4df01f037a8a03ca
EakzIT <pilpaff@gmail.com>
2020-11-28 21:34:24 +0200

    HR advanced interview preparation kit greedy algorithms JS solution :vhs:

---
+++ hackerrank/Interview_preparation_kit/greedy_algorithms/reverse_shuffle_merge.js
@@ -0,0 +1,74 @@
+// www.hackerrank.com/challenges/reverse-shuffle-merge/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on("end", _ => {
+    inputString = inputString
+        .replace(/\s*$/, "")
+        .split("\n")
+        .map(str => str.replace(/\s*$/, ""));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function reverseShuffleMerge(s) {
+    let map = {};
+    s = s.split("").reverse("").join("");
+    for (let item of s.split("")) {
+        map[item] = map[item] ? map[item] + 1 : 1;
+    }
+    let ref = {};
+    for (let key in map) {
+        ref[key] = map[key] / 2;
+    }
+    let solution = [],
+        i = 0;
+    while (solution.length < s.length / 2) {
+        let min_char_pos = -1;
+        while (true) {
+            let c = s[i];
+            if (ref[c] > 0 && (min_char_pos < 0 || c < s[min_char_pos])) {
+                min_char_pos = i;
+            }
+            map[c] -= 1;
+            if (map[c] < ref[c]) {
+                break;
+            }
+            i += 1;
+        }
+        for (let j = min_char_pos + 1; j < i + 1; j++) {
+            map[s[j]] += 1;
+        }
+        ref[s[min_char_pos]] -= 1;
+        solution.push(s[min_char_pos]);
+        i = min_char_pos + 1;
+    }
+    return solution.join("");
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = reverseShuffleMerge(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
