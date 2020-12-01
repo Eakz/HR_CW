cbb41aba769ab257d4e7513335b5296eb41a1f6c
EakzIT <pilpaff@gmail.com>
2020-12-01 20:27:24 +0200

    fixing dumb spelling for DRY bitch :scream:

--- hackerrank/problem_solving/algorithms/implementation/viral_advertising.js
+++ hackerrank/problem_solving/algorithms/implementation/viral_advertising.js
@@ -1,6 +1,7 @@
 const viralAdvertising = (n, t = 5, c = 0) => {
-    if (n === 1) return c + Math.floor(t / 2);
-    return viralAdvertising(n - 1, Math.floor(t / 2) * 3, c + Math.floor(t / 2));
+    let like = Math.floor(t / 2);
+    if (n === 1) return c + like;
+    return viralAdvertising(n - 1, like * 3, c + like);
 };
 console.log(viralAdvertising(4), "===", 15);
 console.log(viralAdvertising(3), "===", 9);
