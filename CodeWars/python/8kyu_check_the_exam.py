a1ab330c50116051349264bbf95aa64a0c01d88f
EakzIT <pilpaff@gmail.com>
2020-12-04 21:00:06 +0200

    CW py 8kyu check the examp :vhs:

---
+++ CodeWars/python/8kyu_check_the_exam.py
@@ -0,0 +1,11 @@
+YP  # https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/python
+
+
+def check_exam(arr1, arr2):
+    return max(sum([4 if arr1[i] == arr2[i] else -1 if len(arr2[i]) == 1 else 0 for i in range(len(arr1))]), 0)
+
+
+print(check_exam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6)
+print(check_exam(["a", "a", "c", "b"], ["a", "a", "b",  ""]), 7)
+print(check_exam(["a", "a", "b", "c"], ["a", "a", "b", "c"]), 16)
+print(check_exam(["b", "c", "b", "a"], ["",  "a", "a", "c"]), 0)
