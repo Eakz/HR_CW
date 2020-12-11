ae32aa223d84261f95cbcf60a2569b1801b50703
EakzIT <pilpaff@gmail.com>
2020-12-11 21:01:29 +0200

    CW 8kyu reversed string JS solution :vhs:

---
+++ CodeWars/JS/8kyu_reversed_strings.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+const solution = s => s.split("").reverse().join("");
+
+console.log(solution("world"), "dlrow");
+console.log(solution("hello"), "olleh");
+console.log(solution(""), "");
+console.log(solution("h"), "h");
