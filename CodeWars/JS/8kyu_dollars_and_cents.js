38c0acb4795f1af46c4af434311276ac0daa1d6b
EakzIT <pilpaff@gmail.com>
2020-12-21 14:37:09 +0200

    CW 8kyu dollars and cents JS solution :vhs:

---
+++ CodeWars/JS/8kyu_dollars_and_cents.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55902c5eaa8069a5b4000083/train/javascript
+
+const formatMoney = a => "$" + a.toFixed(2);
+
+console.log(formatMoney(39.99), "$39.99", "That's not formatted the way we expected.");
