041ee8d204770f903b721df25755b1d3223c40e4
eakz <pilpaff@gmail.com>
2021-01-06 18:34:56 +0200

    CW 8kyu JS

---
+++ CodeWars/JS/8kyu_find_the_position.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascripthttps://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascript
+
+const position = l =>`Position of alphabet: ${l.charCodeAt()-96}`
+
+console.log(position("a"),"Position of alphabet: 1");
+console.log(position("z"),"Position of alphabet: 26");
+console.log(position("e"),"Position of alphabet: 5");
