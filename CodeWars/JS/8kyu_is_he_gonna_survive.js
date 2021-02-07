9eddc6df7ced96fcf4405cba07cc17f8ce1acb83
eakz <pilpaff@gmail.com>
2021-02-07 11:54:59 +0200

    CW 8kyu JS :vhs:

---
+++ CodeWars/JS/8kyu_is_he_gonna_survive.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+hero=(b,d)=>b/2>=d
+
+console.log(hero(10, 5), true);
+console.log(hero(7, 4), false);
+console.log(hero(4, 5), false);
+console.log(hero(100, 40), true);
+console.log(hero(1500, 751), false);
+console.log(hero(0, 1), false);
+        
