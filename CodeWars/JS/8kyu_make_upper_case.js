cec4ae131027d75f8a463e993114b71ab4137973
eakz <pilpaff@gmail.com>
2021-02-25 19:19:45 +0200

    CW 8kyu - because lazy :vhs: JS

---
+++ CodeWars/JS/8kyu_make_upper_case.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
+
+console.log(makeUpperCase("hello"), "HELLO");
