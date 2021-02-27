e9dee689b4c86c52e68956bdb7a8aa18676bb21a
EakzIT <pilpaff@gmail.com>
2021-02-27 05:16:54 +0200

    CW 8kyu JS solution :vhs:

---
+++ CodeWars/JS/8kyu_convert_boolean_values_to_strings.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+function boolToWord( bool ){
+  return bool?'Yes':'No'
+}
+console.log(boolToWord(true), 'Yes')
+console.log(boolToWord(false), 'No')
