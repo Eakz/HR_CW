b4667fa1b9eafbe5b2ff7f4a9459e3fd3e67a023
EakzIT <pilpaff@gmail.com>
2021-03-02 17:49:13 -0600

    CW 8kyu JS :vhs:

---
+++ CodeWars/JS/8kyu_return_negative.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+    return Math.abs(num)*-1
+  }
+  Test.assertEquals(makeNegative(42), -42);
