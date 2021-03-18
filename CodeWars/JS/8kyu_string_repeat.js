3beaf4cefd2ba0ef8951ff845e891f1e0395f7dc
eakz <pilpaff@gmail.com>
2021-03-18 21:13:16 +0200

    CW 8kyu :tooDumb:

---
+++ CodeWars/JS/8kyu_string_repeat.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+function repeatStr (n, s) {
+    return s.repeat(n)
+  }
+
+
+console.log(repeatStr(3, "*"), "***");
+console.log(repeatStr(5, "#"), "#####");
+console.log(repeatStr(2, "ha "), "ha ha ");
