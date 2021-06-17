bc6d256a291268443ff398a46bcebaf3e037da8e
eakz <pilpaff@gmail.com>
2021-06-17 22:50:43 +0300

    CW 7kyu JS solution

---
+++ CodeWars/JS/7kyu_what_a_classy_song.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/6089c7992df556001253ba7d/train/javascript
+
+class Song {
+    constructor(title,artist){
+        this.title=title;
+        this.artist=artist
+        this.participants=[]
+    }
+    howMany(a){
+        let filtered = [...new Set(a.map(e=>e.toLowerCase()).filter(el=>!this.participants.includes(el)))]
+        this.participants=[...this.participants,...filtered]
+        return filtered.length
+    }
+}
