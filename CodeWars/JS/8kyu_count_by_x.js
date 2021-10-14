67457a7fff300a3bd6e0acc3926ff91dde9e307a
eakz <pilpaff@gmail.com>
2021-10-14 22:38:46 +0300

    CW 8kyu  JS

--- CodeWars/JS/8kyu_count_by_x.js
+++ CodeWars/JS/8kyu_count_by_x.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
 
 countBy=(x,n)=>[...Object.keys(Array(n+1).fill(''))].slice(1).map(e=>e*x)
-
+const countBy = (x, n) => Array.from({length: n}, (v, k) => (k + 1) * x)
 
 console.log(countBy(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Array does not match")
 console.log(countBy(2, 5), [2, 4, 6, 8, 10], "Array does not match")
