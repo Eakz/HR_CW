2e5b6fd3f84a57cf2f6d6469361c9388e18b92c5
Eakz <pilpaff@gmail.com>
2022-01-22 17:53:42 +0200

    CW 6kyu JS solution

---
+++ CodeWars/JS/6kyu_simple_fun_247_to_and_from.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/590c3173cd3b99c467000a26/train/javascript
+const assert = require('chai').assert;
+const toAndFrom = (a, b, t) => {
+    const diff = b - a;
+    const pos = t % diff;
+    const post = diff < 0 ? a - pos : a + pos;
+    const altpost = diff < 0 ? b + pos : b - pos;
+    const isAlternate = ((t / diff) | 0) % 2 !== 0;
+    return isAlternate ? altpost : post;
+};
+
+if (process.mainModule.path.includes('mocha')) {
+    describe('Basic Tests', function () {
+        it('It should works for basic tests.', function () {
+            assert.equal(toAndFrom(2, 4, 3), 3);
+
+            assert.equal(toAndFrom(1, 10, 8), 9);
+
+            assert.equal(toAndFrom(10, 4, 8), 6);
+
+            assert.equal(toAndFrom(2, 4, 5), 3);
+
+            assert.equal(toAndFrom(42, 150, 548), 142);
+
+            assert.equal(toAndFrom(75, 9, 6), 69);
+        });
+    });
+} else {
+    console.log(toAndFrom(2, 4, 3), 3);
+    console.log(toAndFrom(1, 10, 8), 9);
+    console.log(toAndFrom(10, 4, 8), 6);
+    console.log(toAndFrom(2, 4, 5), 3);
+    console.log(toAndFrom(42, 150, 548), 142);
+    console.log(toAndFrom(75, 9, 6), 69);
+}
--- CodeWars/JS/8kyu_sum_the_string.js
+++ CodeWars/JS/8kyu_sum_the_string.js
@@ -1,7 +1,7 @@
 // Offtopic as of lately due to holidays and some exhaustion of coding I tend to do easy tasks right now
 // https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+const Test = require('chai')
 
 const sumStr = (a, b) => eval(`${a || 0}+${b || 0}`).toString();
-
 Test.assertEquals(sumStr("4", "5"), "9");
 Test.assertEquals(sumStr("34", "5"), "39");
