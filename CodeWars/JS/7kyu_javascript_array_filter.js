8b1b7ea2c87a6db39052b78354d8956f6f22a162
Eakz <pilpaff@gmail.com>
2022-01-24 19:40:50 +0200

    CW 7kyu JS solution

---
+++ CodeWars/JS/7kyu_javascript_array_filter.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript
+const assert = require('chai').assert;
+function getEvenNumbers(numbersArray) {
+    return numbersArray.filter(e=>e%2===0)
+}
+
+console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]), [2, 6, 8, 10]);
+console.log(getEvenNumbers([1, 2]), [2]);
+console.log(getEvenNumbers([12, 14, 15]), [12, 14]);
+console.log(getEvenNumbers([13, 15]), []);
+console.log(getEvenNumbers([1, 3, 9]), []);
+
+if (process.mainModule.path.includes('mocha')) {
+    
+
+    describe('Fixed tests', () => {
+        it('should return an empty array if there are no even numbers', function () {
+            assert.deepEqual(getEvenNumbers([1, 2, 3, 6, 8, 10]), [2, 6, 8, 10]);
+            assert.deepEqual(getEvenNumbers([1, 2]), [2]);
+            assert.deepEqual(getEvenNumbers([12, 14, 15]), [12, 14]);
+            assert.deepEqual(getEvenNumbers([13, 15]), []);
+            assert.deepEqual(getEvenNumbers([1, 3, 9]), []);
+        });
+    });
+}
