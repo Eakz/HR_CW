29f6d1739bfd143a9eccabb8dea3cfbd22ee9c12
Eakz <pilpaff@gmail.com>
2022-02-04 09:01:34 -0600

    CW 6kyu JS

---
+++ CodeWars/JS/6kyu_playing_with_digits.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/5552101f47fc5178b1000050/train/javascript
+
+function digPow(n, p) {
+    const leftSide = [...n.toString()].reduce((acc, e, i) => acc + e ** (p + i), 0);
+    const result = leftSide / n;
+
+    return result === (result | 0) ? result : -1;
+}
+console.log(digPow(89, 1), 1);
+console.log(digPow(92, 1), -1);
+console.log(digPow(46288, 3), 51);
+
+if (process.mainModule.path.includes('mocha')) {
+    const Test = require('chai').assert;
+    describe('Tests', () => {
+        it('test', () => {
+            Test.equal(digPow(89, 1), 1);
+            Test.equal(digPow(92, 1), -1);
+            Test.equal(digPow(46288, 3), 51);
+        });
+    });
+}
