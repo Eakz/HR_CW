68bb5f8ee352713387e5d3f96b4a94a8315c73ea
Eakz <pilpaff@gmail.com>
2022-02-08 22:44:05 +0200

    CW 8kyu (sorry too tired to do other)

---
+++ CodeWars/JS/8kyu_area_or_perimeter.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/javascript
+
+const areaOrPerimeter = function(l , w) {
+    return l===w?l*w:2*(l+w)
+  };
+
+  if (process.mainModule.path.includes('mocha')) {
+const Test = require('chai').assert
+  describe("Tests", () => {
+    it("test", () => {
+  Test.equal(areaOrPerimeter(4 , 4) , 16);
+  Test.equal(areaOrPerimeter(6 , 10) , 32);
+    });
+  });
+}
