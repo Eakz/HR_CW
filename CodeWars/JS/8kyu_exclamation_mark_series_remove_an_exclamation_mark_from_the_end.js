bdd0984421a1991fd071ec6ff0d040ef8b1469bb
Eakz <pilpaff@gmail.com>
2022-02-12 20:56:20 +0200

    CW 8kyu JS

---
+++ CodeWars/JS/8kyu_exclamation_mark_series_remove_an_exclamation_mark_from_the_end.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/57fae964d80daa229d000126/train/javascript
+
+function remove(string) {
+    return string.slice(-1) === '!' ? string.slice(0, -1) : string;
+}
+
+// Regex option
+// const remove = s => s.replace(/!$/, '');
+
+if (process.mainModule.path.includes('mocha')) {
+    const strictEqual = require('chai').assert.strictEqual;
+    function doTest(input, expected) {
+        const actual = remove(input);
+        strictEqual(actual, expected, `for string:\n"${input}"\n`);
+    }
+
+    describe('Tests suite', function () {
+        it('sample tests', function () {
+            doTest('Hi!', 'Hi');
+            doTest('Hi!!!', 'Hi!!');
+            doTest('!Hi', '!Hi');
+            doTest('!Hi!', '!Hi');
+            doTest('Hi! Hi!', 'Hi! Hi');
+            doTest('Hi', 'Hi');
+        });
+    });
+}
