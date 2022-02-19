9928354cde595e77039d5af1413a64e8e3686866
Eakz <pilpaff@gmail.com>
2022-02-19 14:33:27 +0200

    7kyu JS solution CW

---
+++ CodeWars/JS/7kyu_is_n_divisible_by.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/558ee8415872565824000007/train/javascript
+
+const isDivisible = (n, ...rest) => rest.every(e => n % e === 0);
+
+if (process.mainModule.path.includes('mocha')) {
+    const Test = require('chai').assert;
+    describe('Tests', () => {
+        it('test', () => {
+            Test.equal(isDivisible(3, 3, 4), false);
+            Test.equal(isDivisible(12, 3, 4), true);
+            Test.equal(isDivisible(8, 3, 4, 2, 5), false);
+        });
+    });
+}
