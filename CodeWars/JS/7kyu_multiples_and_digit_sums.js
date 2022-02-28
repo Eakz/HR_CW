26f992df381e67f8cc47bb817956f0ddcb793f99
Eakz <pilpaff@gmail.com>
2022-02-28 20:31:10 +0200

    CW 7kyu JS - wartime brainfarts

---
+++ CodeWars/JS/7kyu_multiples_and_digit_sums.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/58ca77b9c0d640ecd2000b1e/train/javascript
+const procedure = n =>
+    Array((100 / n) | 0)
+        .fill('')
+        .reduce(
+            (acc, _, i) =>
+                acc +
+                [...`${(i + 1) * n}`].reduce((acc2, e) => Number.parseInt(e) + acc2, 0),
+            0,
+        );
+
+console.log(procedure(30), 18);
+console.log(procedure(12), 72);
+console.log(procedure(49), 30);
+console.log(procedure(17), 48);
+console.log(procedure(10), 46);
+
+if (process.mainModule.path.includes('mocha')) {
+    const Test = require('chai');
+    describe('Solution', function () {
+        it('Fixed Test', function () {
+            Test.assert.equal(procedure(30), 18);
+            Test.assert.equal(procedure(12), 72);
+            Test.assert.equal(procedure(49), 30);
+            Test.assert.equal(procedure(17), 48);
+            Test.assert.equal(procedure(10), 46);
+        });
+    });
+}
