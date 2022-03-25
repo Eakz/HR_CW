ebca3f44df701f72995f6696ccf9108cdc967a08
Pavel Piliuiko <35857358+Eakz@users.noreply.github.com>
2022-03-25 13:38:52 +0200

    CW 7kyu JS

---
+++ CodeWars/JS/7kyu_all_inclusive.js
@@ -0,0 +1,44 @@
+// https://www.codewars.com/kata/5700c9acc1555755be00027e/train/javascript
+
+function containAllRots(strng, arr) {
+    const defaultStr = strng + strng;
+    for (let i = 0; i < strng.length; i++) {
+        const currentWord = defaultStr.slice(i, i + strng.length);
+        if (!arr.includes(currentWord)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+if (process.mainModule.path.includes('mocha')) {
+    const Test = require('chai').assert;
+    function testing(actual, expected) {
+        Test.equal(actual, expected);
+    }
+
+    describe('Basic tests', function () {
+        it('containAllRots', function () {
+            testing(containAllRots('', []), true);
+            testing(containAllRots('', ['bsjq', 'qbsj']), true);
+            testing(
+                containAllRots('bsjq', ['bsjq', 'qbsj', 'sjqb', 'twZNsslC', 'jqbs']),
+                true,
+            );
+            testing(
+                containAllRots('XjYABhR', [
+                    'TzYxlgfnhf',
+                    'yqVAuoLjMLy',
+                    'BhRXjYA',
+                    'YABhRXj',
+                    'hRXjYAB',
+                    'jYABhRX',
+                    'XjYABhR',
+                    'ABhRXjY',
+                ]),
+                false,
+            );
+        });
+    });
+}
