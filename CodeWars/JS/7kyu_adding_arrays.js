a4e2b31d3b41fcd01af440d54890125b5f450515
Pavel Piliuiko <35857358+Eakz@users.noreply.github.com>
2022-04-10 22:00:59 +0300

    CW 7kyu JS

---
+++ CodeWars/JS/7kyu_adding_arrays.js
@@ -0,0 +1,67 @@
+// https://www.codewars.com/kata/59778cb1b061e877c50000cc
+
+function arrAdder(arr) {
+   return arr[0].map((e,i)=>([e,...arr.slice(1).map(el=>el[i])].join(''))).join(' ')
+}
+if (process.mainModule.path.includes('mocha')) {
+    const Test = require('chai').assert;
+    describe('Tests', () => {
+        it('test', () => {
+            Test.equal(
+                arrAdder([
+                    ['J', 'L', 'L', 'M'],
+                    ['u', 'i', 'i', 'a'],
+                    ['s', 'v', 'f', 'n'],
+                    ['t', 'e', 'e', ''],
+                ]),
+                'Just Live Life Man',
+            );
+
+            Test.equal(
+                arrAdder([
+                    ['T', 'M', 'i', 't', 'p', 'o', 't', 'c'],
+                    ['h', 'i', 's', 'h', 'o', 'f', 'h', 'e'],
+                    ['e', 't', '', 'e', 'w', '', 'e', 'l'],
+                    ['', 'o', '', '', 'e', '', '', 'l'],
+                    ['', 'c', '', '', 'r', '', '', ''],
+                    ['', 'h', '', '', 'h', '', '', ''],
+                    ['', 'o', '', '', 'o', '', '', ''],
+                    ['', 'n', '', '', 'u', '', '', ''],
+                    ['', 'd', '', '', 's', '', '', ''],
+                    ['', 'r', '', '', 'e', '', '', ''],
+                    ['', 'i', '', '', '', '', '', ''],
+                    ['', 'a', '', '', '', '', '', ''],
+                ]),
+                'The Mitochondria is the powerhouse of the cell',
+            );
+        });
+    });
+} else {
+    console.log(
+        arrAdder([
+            ['J', 'L', 'L', 'M'],
+            ['u', 'i', 'i', 'a'],
+            ['s', 'v', 'f', 'n'],
+            ['t', 'e', 'e', ''],
+        ]),
+        'Just Live Life Man',
+    );
+
+    console.log(
+        arrAdder([
+            ['T', 'M', 'i', 't', 'p', 'o', 't', 'c'],
+            ['h', 'i', 's', 'h', 'o', 'f', 'h', 'e'],
+            ['e', 't', '', 'e', 'w', '', 'e', 'l'],
+            ['', 'o', '', '', 'e', '', '', 'l'],
+            ['', 'c', '', '', 'r', '', '', ''],
+            ['', 'h', '', '', 'h', '', '', ''],
+            ['', 'o', '', '', 'o', '', '', ''],
+            ['', 'n', '', '', 'u', '', '', ''],
+            ['', 'd', '', '', 's', '', '', ''],
+            ['', 'r', '', '', 'e', '', '', ''],
+            ['', 'i', '', '', '', '', '', ''],
+            ['', 'a', '', '', '', '', '', ''],
+        ]),
+        'The Mitochondria is the powerhouse of the cell',
+    );
+}
