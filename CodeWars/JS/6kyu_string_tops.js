f94460ab5f53f148b6019fbe418ed08e4dcf5257
Eakz <pilpaff@gmail.com>
2022-09-04 18:30:34 +0300

    CW 6kyu JS

---
+++ CodeWars/JS/6kyu_string_tops.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/59b7571bbf10a48c75000070/train/javascript
+
+function tops(msg) {
+    let i = 1;
+    let mx = 2;
+    let result = '';
+    const length = msg.length;
+    while (i < length) {
+        result += msg[i];
+        i += mx;
+        mx++;
+    }
+    return [...result].reduce((acc, e, i) => (i % 2 === 1 ? acc : e+ acc), '');
+}
+
+if (process.mainModule.path.includes('mocha')) {
+    const test = require('chai').assert.equal;
+    describe('Tests', () => {
+        it('test', () => {
+            test('', tops(''));
+            test('2', tops('12'));
+            test('3pgb', tops('abcdefghijklmnopqrstuvwxyz12345'));
+            test('M3pgb', tops('abcdefghijklmnopqrstuvwxyz1236789ABCDEFGHIJKLMN'));
+        });
+    });
+} else {
+    console.log(tops(''), '');
+    console.log(tops('12'), '2');
+    console.log(tops('abcdefghijklmnopqrstuvwxyz12345'), '3pgb');
+    console.log(tops('abcdefghijklmnopqrstuvwxyz1236789ABCDEFGHIJKLMN'), 'M3pgb');
+}
+// 1  3 6   10   15   21   28
