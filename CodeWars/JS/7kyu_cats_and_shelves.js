799158ebf5e7bec97d004cd4c89845e3540e5748
Eakz <pilpaff@gmail.com>
2022-10-10 21:50:51 +0300

    CW 7kyu JS

---
+++ CodeWars/JS/7kyu_cats_and_shelves.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/62c93765cef6f10030dfa92b/train/javascript
+
+const solution = (s, f) => {
+    const diff = f - s;
+    const div = diff / 3 | 0;
+    const mod = diff % 3;
+    return div + mod;
+}
+
+if (process.mainModule.path.includes('mocha')) {
+    const chai = require('chai');
+    const assert = chai.assert;
+    chai.config.truncateThreshold = 0;
+
+    describe('Example test cases', function () {
+        it('Test case in description', function () {
+            assert.strictEqual();
+        });
+    });
+} else {
+    console.log(solution(1, 5), 2);
+}
