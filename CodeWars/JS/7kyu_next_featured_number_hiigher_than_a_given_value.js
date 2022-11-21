1e3d2023c8fbda8af5c9ca7a724c6a0927889baf
Eakz <pilpaff@gmail.com>
2022-11-21 12:48:36 +0200

    CW 7kyu

---
+++ CodeWars/JS/7kyu_next_featured_number_hiigher_than_a_given_value.js
@@ -0,0 +1,46 @@
+// https://www.codewars.com/kata/56abc5e63c91630882000057/train/javascript
+
+function nextNumb(val) {
+    if (val.toString().length >= 10)
+        return 'There is no possible number that fulfills those requirements';
+    let coef = Math.ceil(val / 3);
+    let curnext = coef * 3;
+    if(curnext===val) curnext+=3;
+    const evaluate = (v = 0) => {
+        if (v % 2 === 0) return false;
+        const v2StrArr = v.toString().split('');
+        const noduplicatesLen = Array.from(new Set(v2StrArr)).length;
+        if (noduplicatesLen !== v2StrArr.length) return false;
+        return true;
+    };
+    while (true) {
+        if (evaluate(curnext)) {
+            return curnext;
+        }
+        curnext += 3;
+    }
+}
+
+if (process.mainModule.path.includes('mocha')) {
+    describe('Example Tests', function () {
+        Test.assertEquals(nextNumb(12), 15);
+        Test.assertEquals(nextNumb(13), 15);
+        Test.assertEquals(nextNumb(99), 105);
+        Test.assertEquals(nextNumb(999999), 1023459);
+        Test.assertEquals(
+            nextNumb(9999999999),
+            'There is no\
+         possible number that fulfills those requirements',
+        );
+    });
+} else {
+    console.log(nextNumb(12), 15);
+    console.log(nextNumb(13), 15);
+    console.log(nextNumb(99), 105);
+    console.log(nextNumb(999999), 1023459);
+    console.log(
+        nextNumb(9999999999),
+        'There is no\
+     possible number that fulfills those requirements',
+    );
+}
