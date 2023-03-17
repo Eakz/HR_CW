899558b4b96660e42aea2842374b552a0a96f2ba
Eakz <pilpaff@gmail.com>
2023-03-17 18:35:58 +0200

    CW 8kyu JS

---
+++ CodeWars/JS/8kyu_determine_offspring_sex.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56530b444e831334c0000020/train/javascript
+
+function chromosomeCheck(sperm) {
+    return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+  }
