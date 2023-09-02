6d78a8b3273843d8d01363c350daa49519ab5cdb
Eakz <pilpaff@gmail.com>
2023-09-02 15:33:52 +0300

    Leetcode 150

---
+++ leetcode/top150/remove_duplicates_sorted_array.ts
@@ -0,0 +1,15 @@
+const removeDuplicates = (nums: number[]): number => {
+    let j: number = 2;
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[j - 2]) {
+            nums[j++] = nums[i];
+        }
+    }
+
+    return j;
+};
+
+const a = [1, 1, 1, 2, 2, 3];
+removeDuplicates(a);
+console.log(a);
