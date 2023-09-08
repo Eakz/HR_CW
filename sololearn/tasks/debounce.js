e21ae0d45ed5931842a075e77f3e6df6e4cd48a5
Eakz <pilpaff@gmail.com>
2023-09-08 18:47:55 +0300

    sololearn debounce

--- sololearn/tasks/debounce.js
+++ sololearn/tasks/debounce.js
@@ -2,7 +2,7 @@ export default function debounce(func, delay) {
     let timer;
 
     return function (...args) {
-        clearTimeout(timer);<XMLDocumentx>  </XMLDocumentx>
+        clearTimeout(timer);
         timer = setTimeout(() => func.apply(this, args), delay);
     };
 }
