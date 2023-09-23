5c93963beb60b19843521f610ee22439bb09ee03
Eakz <pilpaff@gmail.com>
2023-09-23 11:52:55 +0300

    sololearn algo tasks

---
+++ sololearn/tasks/two_sum.js
@@ -0,0 +1,8 @@
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const need = target - nums[i];
+    if (map.has(need)) return [map.get(need), i];
+    map.set(nums[i], i);
+  }
+}
