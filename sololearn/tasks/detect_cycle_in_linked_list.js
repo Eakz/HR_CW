67d36ba2204c47f1c3619c368080dad267dc6abc
Eakz <pilpaff@gmail.com>
2023-10-06 17:45:15 +0300

    sololearn algo tasks

---
+++ sololearn/tasks/detect_cycle_in_linked_list.js
@@ -0,0 +1,10 @@
+function hasCycle(head) {
+    let slow = head,
+        fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+    return false;
+}
