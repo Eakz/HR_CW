6f17efa7a561898f655c583389380d3fcd093ba7
Eakz <pilpaff@gmail.com>
2023-10-29 16:09:50 +0200

    leetcode fix for range

--- leetcode/top150/reverse_32b_integer.js
+++ leetcode/top150/reverse_32b_integer.js
@@ -16,9 +16,15 @@
  * @return {number}
  */
 var reverse = function (x) {
+    if(!this.maxInt && !this.minInt){
+        const maxInt = Math.pow(2,31);
+        const minInt = -1*maxInt;
+        this.maxInt=maxInt;
+        this.minInt=minInt;
+    }
     const sign = x < 0 ? '-' : '';
     const reversed = parseInt(sign + `${x}`.split('').reverse().join(''));
-    return (reversed | 0 === reversed) ? reversed : 0;
+    return reversed<=this.maxInt && reversed>=this.minInt && (reversed|0===reversed)?reversed:0
 };
 
 console.log(reverse(123), 321);
