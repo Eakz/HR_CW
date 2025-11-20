34b1e625cfd0c48544d93dd4ba9955c8df7e7890
Eakz <pilpaff@gmail.com>
2025-11-20 18:37:38 +0200

    go words prep

---
+++ selfstudy/golang/words_sortasc.go
@@ -0,0 +1,47 @@
+/*
+task 299:
+Words - SortAsc
+You need to sort the values in ascending order for word lengths
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Sort_upWords(lengths []int) []int {
+	res := make([]int, len(lengths))
+	copy(res, lengths)
+	for i := 0; i < len(res); i++ {
+		for j := i+1; j < len(res); j++ {
+			if res[i] > res[j] { res[i], res[j] = res[j], res[i] }
+		}
+	}
+	return res
+}
+
+func main() {
+	tests := []struct {
+		input []int
+		expected []int
+	}{
+		{[]int{10, 20, 30}, Sort_upWords([]int{10, 20, 30})},
+		{[]int{50, 60, 70}, Sort_upWords([]int{50, 60, 70})},
+		{[]int{1, 2, 3}, Sort_upWords([]int{1, 2, 3})},
+		{[]int{100, 200}, Sort_upWords([]int{100, 200})},
+		{[]int{}, Sort_upWords([]int{})},
+	}
+	
+	for idx, t := range tests {
+		res := Sort_upWords(t.input)
+		status := "FAIL"
+		if reflect.DeepEqual(res, t.expected) {
+			status = "PASS"
+		}
+		fmt.Printf("Test %d: %s\n", idx+1, status)
+	}
+}
+
+// finally works
