509916ba07e38eb5d2da6e8819ed6bf116685a67
Eakz <pilpaff@gmail.com>
2025-11-28 18:44:47 +0200

    did cars counthigh ik

---
+++ selfstudy/golang/cars_counthigh.go
@@ -0,0 +1,44 @@
+/*
+task 227:
+Cars - CountHigh
+You need to count how many values are greater than 50 in car speeds in km/h
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Count_above_fiftyCars(speeds []int) int {
+	count := 0
+	for _, v := range speeds {
+		if v > 50 { count++ }
+	}
+	return count
+}
+
+func main() {
+	tests := []struct {
+		input []int
+		expected int
+	}{
+		{[]int{10, 20, 30}, Count_above_fiftyCars([]int{10, 20, 30})},
+		{[]int{50, 60, 70}, Count_above_fiftyCars([]int{50, 60, 70})},
+		{[]int{1, 2, 3}, Count_above_fiftyCars([]int{1, 2, 3})},
+		{[]int{100, 200}, Count_above_fiftyCars([]int{100, 200})},
+		{[]int{}, Count_above_fiftyCars([]int{})},
+	}
+	
+	for idx, t := range tests {
+		res := Count_above_fiftyCars(t.input)
+		status := "FAIL"
+		if reflect.DeepEqual(res, t.expected) {
+			status = "PASS"
+		}
+		fmt.Printf("Test %d: %s\n", idx+1, status)
+	}
+}
+
+// works i think
