140333c9aadf8e9b55184c068cf528536c29e070
Eakz <pilpaff@gmail.com>
2025-12-18 20:06:59 +0200

    finished python cars

---
+++ selfstudy/python/cars_filterlow.py
@@ -0,0 +1,28 @@
+'''
+task 126:
+Cars - FilterLow
+You need to return only the values less than 50 from car speeds in km/h
+'''
+
+def below_fifty_cars(speeds):
+    return [x for x in speeds if x < 50]
+
+# Tests
+if __name__ == '__main__':
+    tests = [
+        ([10, 20, 30], below_fifty_cars([10, 20, 30])),
+        ([50, 60, 70], below_fifty_cars([50, 60, 70])),
+        ([1, 2, 3], below_fifty_cars([1, 2, 3])),
+        ([100, 200], below_fifty_cars([100, 200])),
+        ([], below_fifty_cars([]))
+    ]
+    import sys
+    if 'pytest' in sys.argv:
+        # pytest structure
+        pass
+    else:
+        for idx, (inp, exp) in enumerate(tests):
+            res = below_fifty_cars(inp)
+            print(f"Test {idx+1}:", "PASS" if res == exp else "FAIL")
+
+# done
