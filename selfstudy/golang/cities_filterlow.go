d3b4f4cbed12922cc0d42a918ea686ee5c07d204
Eakz <pilpaff@gmail.com>
2025-12-25 21:19:00 +0200

    done with ctiies filterow kata

---
+++ selfstudy/golang/cities_filterlow.go
@@ -0,0 +1,44 @@
+/*
+task 276:
+Cities - FilterLow
+You need to return only the values less than 50 from city populations in thousands
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Below_fiftyCities(populations []int) []int {
+	var res []int
+	for _, v := range populations {
+		if v < 50 { res = append(res, v) }
+	}
+	return res
+}
+
+func main() {
+	tests := []struct {
+		input []int
+		expected []int
+	}{
+		{[]int{10, 20, 30}, Below_fiftyCities([]int{10, 20, 30})},
+		{[]int{50, 60, 70}, Below_fiftyCities([]int{50, 60, 70})},
+		{[]int{1, 2, 3}, Below_fiftyCities([]int{1, 2, 3})},
+		{[]int{100, 200}, Below_fiftyCities([]int{100, 200})},
+		{[]int{}, Below_fiftyCities([]int{})},
+	}
+	
+	for idx, t := range tests {
+		res := Below_fiftyCities(t.input)
+		status := "FAIL"
+		if reflect.DeepEqual(res, t.expected) {
+			status = "PASS"
+		}
+		fmt.Printf("Test %d: %s\n", idx+1, status)
+	}
+}
+
+// works i think
