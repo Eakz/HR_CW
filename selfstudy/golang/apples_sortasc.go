a32798a498c377cf103abfa4d64501a1646978fd
Eakz <pilpaff@gmail.com>
2026-03-22 19:41:22 +0200

    fially paples sortasc logic

---
+++ selfstudy/golang/apples_sortasc.go
@@ -0,0 +1,47 @@
+/*
+task 209:
+Apples - SortAsc
+You need to sort the values in ascending order for apple weights in grams
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Sort_upApples(apples []int) []int {
+	res := make([]int, len(apples))
+	copy(res, apples)
+	for i := 0; i < len(res); i++ {
+		for j := i+1; j < len(res); j++ {
+			if res[i] > res[j] { res[i], res[j] = res[j], res[i] }
+		}
+	}
+	return res
+}
+
+func main() {
+	tests := []struct {
+		input []int
+		expected []int
+	}{
+		{[]int{10, 20, 30}, Sort_upApples([]int{10, 20, 30})},
+		{[]int{50, 60, 70}, Sort_upApples([]int{50, 60, 70})},
+		{[]int{1, 2, 3}, Sort_upApples([]int{1, 2, 3})},
+		{[]int{100, 200}, Sort_upApples([]int{100, 200})},
+		{[]int{}, Sort_upApples([]int{})},
+	}
+	
+	for idx, t := range tests {
+		res := Sort_upApples(t.input)
+		status := "FAIL"
+		if reflect.DeepEqual(res, t.expected) {
+			status = "PASS"
+		}
+		fmt.Printf("Test %d: %s\n", idx+1, status)
+	}
+}
+
+// works i think
