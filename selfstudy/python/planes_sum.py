d2685522237d250fe1a78294657aa28dfa400da3
Eakz <pilpaff@gmail.com>
2026-06-19 19:49:54 +0300

    wip python plaens prep

---
+++ selfstudy/python/planes_sum.py
@@ -0,0 +1,23 @@
+'''
+Task: Planes - Sum
+Calculate the total sum of plane speeds.
+'''
+
+def sum_planes(planes):
+    return sum(planes)
+
+if __name__ == '__main__':
+    tests = [
+        ([10, 20, 30], 60),
+        ([5, 5, 5], 15),
+        ([1, 2], 3),
+        ([100], 100),
+        ([], 0)
+    ]
+    import sys
+    if 'pytest' in sys.argv:
+        pass
+    else:
+        for idx, (inp, exp) in enumerate(tests):
+            res = sum_planes(inp)
+            print(f"Test {idx+1}:", "PASS" if res == exp else "FAIL")
