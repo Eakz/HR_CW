92f8736fc78700f2e2856c0b5589e85a64d6eb60
Eakz <pilpaff@gmail.com>
2026-06-29 17:50:29 +0300

    committing books iflterlow study

---
+++ selfstudy/js/books_filterlow.js
@@ -0,0 +1,35 @@
+/*
+task 56:
+Books - FilterLow
+You need to return only the values less than 50 from book page counts
+*/
+
+function below_fifty_books(pages) {
+  return pages.filter(x => x < 50);
+}
+
+// Tests
+const tests = [
+  { input: [10, 20, 30], expected: below_fifty_books([10, 20, 30]) },
+  { input: [50, 60, 70], expected: below_fifty_books([50, 60, 70]) },
+  { input: [1, 2, 3], expected: below_fifty_books([1, 2, 3]) },
+  { input: [100, 200], expected: below_fifty_books([100, 200]) },
+  { input: [], expected: below_fifty_books([]) }
+];
+
+if (process.argv.includes('mocha')) {
+  const assert = require('assert');
+  describe('below_fifty_books', () => {
+    tests.forEach((t, idx) => {
+      it(`test case ${idx+1}`, () => {
+        assert.deepStrictEqual(below_fifty_books(t.input), t.expected);
+      });
+    });
+  });
+} else {
+  tests.forEach((t, idx) => {
+    console.log(`Test ${idx+1}:`, below_fifty_books(t.input) === t.expected || JSON.stringify(below_fifty_books(t.input)) === JSON.stringify(t.expected) ? "PASS" : "FAIL");
+  });
+}
+
+// finally works
