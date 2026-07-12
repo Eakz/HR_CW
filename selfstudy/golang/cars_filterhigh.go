1cd8de6c2445e4c00cf3a248e7e3fbef6d270005
Eakz <pilpaff@gmail.com>
2026-07-12 19:22:25 +0300

    working on cars_filterhigh in go loic

---
+++ selfstudy/golang/cars_filterhigh.go
@@ -0,0 +1,44 @@
+/*
+task 225:
+Cars - FilterHigh
+You need to return only the values greater than 50 from car speeds in km/h
+*/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Above_fiftyCars(speeds []int) []int {
+	var res []int
+	for _, v := range speeds {
+		if v > 50 { res = append(res, v) }
+	}
+	return res
+}
+
+func main() {
+	tests := []struct {
+		input []int
+		expected []int
+	}{
+		{[]int{10, 20, 30}, Above_fiftyCars([]int{10, 20, 30})},
+		{[]int{50, 60, 70}, Above_fiftyCars([]int{50, 60, 70})},
+		{[]int{1, 2, 3}, Above_fiftyCars([]int{1, 2, 3})},
+		{[]int{100, 200}, Above_fiftyCars([]int{100, 200})},
+		{[]int{}, Above_fiftyCars([]int{})},
+	}
+	
+	for idx, t := range tests {
+		res := Above_fiftyCars(t.input)
+		status := "FAIL"
+		if reflect.DeepEqual(res, t.expected) {
+			status = "PASS"
+		}
+		fmt.Printf("Test %d: %s\n", idx+1, status)
+	}
+}
+
+// finally works
